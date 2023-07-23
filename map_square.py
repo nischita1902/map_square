7bf698600e12ed51e047097d922d0fbc20f0bb3c
nischita1902 <133357381+nischita1902@users.noreply.github.com>
2023-07-23 22:07:20 +0530

    Create map_square.py

---
+++ map_square.py
@@ -0,0 +1,5 @@
+sample_list = [4,5,2,9] 
+squared_numbers = list(map(lambda x: x **2, sample_list))
+
+print("Original list:", sample_list)
+print("Square the elements of the list:", squared_numbers)
